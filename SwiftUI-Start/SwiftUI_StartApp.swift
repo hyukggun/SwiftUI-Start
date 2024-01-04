a5a6bfbd641379680d535e7823152aaeb8dbb2ac
hyukggun <hyukggunchoe@gmail.com>
2024-01-04 22:34:09 +0900

    Initial Commit

---
+++ SwiftUI-Start/SwiftUI_StartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StartApp.swift
+//  SwiftUI-Start
+//
+//  Created by 최최광현 on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
